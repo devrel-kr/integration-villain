7abf12ac5d68a07eee2ce9233eea9dbc2dd96f9d
Justin Yoo <justin.yoojh@gmail.com>
2021-11-29 02:46:22 +0900

    Update Codespaces settings

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -7,7 +7,7 @@ git config --global pull.rebase false
 git config --global core.autocrlf false
 
 ## Install node.js 16 LTS
-nvm install --lts Gallium
+sudo nvm install --lts Gallium
 
 ## Install .NET
 sudo wget https://packages.microsoft.com/config/ubuntu/20.10/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
