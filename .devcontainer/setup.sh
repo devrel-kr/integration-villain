46c647684fd7f895b7fae719bc1272674177bba4
Justin Yoo <justin.yoojh@gmail.com>
2021-11-28 16:54:45 +0000

    Update Codespaces settings

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,20 +1,26 @@
+## Install apt packages
+sudo apt-get update && \
+sudo apt-get install -y dos2unix
+
 ## Configure git
 sudo git config --global pull.rebase false
 sudo git config --global core.autocrlf false
 
 ## Install .NET 6
-# sudo wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb && \
-# sudo dpkg -i packages-microsoft-prod.deb && \
-# sudo rm packages-microsoft-prod.deb && \
-# sudo apt-get install -y dotnet-sdk-6.0
+wget https://packages.microsoft.com/config/ubuntu/20.10/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+rm packages-microsoft-prod.deb
+
+sudo apt-get update && \
+sudo apt-get install -y dotnet-sdk-6.0
 
 ## Install Azure Functions Core Tools v4
 # curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 # sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
 # sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-ubuntu-$(lsb_release -cs)-prod $(lsb_release -cs) main" > /etc/apt/sources.list.d/dotnetdev.list'
-# sudo apt-get update
+
+# sudo apt-get update && \
 # sudo apt-get install -y azure-functions-core-tools-4
 
 ## Enable local HTTPS for .NET
 sudo dotnet dev-certs https --trust
-
